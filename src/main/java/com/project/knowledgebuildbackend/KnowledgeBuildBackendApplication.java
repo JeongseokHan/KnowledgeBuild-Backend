8099d19642c380f6bb58528a2bcd6a55334c9789
JeongseokHan <115478278+JeongseokHan@users.noreply.github.com>
2025-03-25 19:18:39 +0900

    프로젝트 생성
    KnowledgeBuild(백엔드) 프로젝트 생성

---
+++ src/main/java/com/project/knowledgebuildbackend/KnowledgeBuildBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.knowledgebuildbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeBuildBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowledgeBuildBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/knowledgebuildbackend/KnowledgeBuildBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.knowledgebuildbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeBuildBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
